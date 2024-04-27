d69971cdce2b8544b5915c6b987ffafd1c356ea8
Guilherme Dias <guilhermediasthomaz@hotmail.com>
2024-04-27 12:24:59 -0300

    Moving pieces

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -46,6 +46,21 @@ public class Board {
 		
 	}
 	
+	public Piece removePiece(Position position) {
+		if(!positionExists(position)) {
+			System.out.println("Position not on the board");
+		}
+		if(piece(position) == null) {
+			return null;
+		}
+		else {
+			Piece aux = piece(position);
+			aux.position = null;
+			pieces[position.getRow()][position.getColumn()] = null;
+			return aux;
+		}
+	}
+	
 	private boolean positionExists(int row, int column) {
 		return row >= 0 && row < rows && column >= 0 && column < columns;
 	}
@@ -60,7 +75,6 @@ public class Board {
 		}
 		return piece(position) != null;
 	}
-	
 	
 	
 }
